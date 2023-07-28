00ec3c8a36ccbd1c3ca726b5ab2cdcb3d67f13be
Nightcycle <77173389+nightcycle@users.noreply.github.com>
2023-07-27 19:27:27 -0600

    removed unnecessary style guide

--- out/StyleGuide/Packages/ColdFusion.lua
+++
@@ -1,6 +0,0 @@
-local REQUIRED_MODULE = require(script.Parent._Index["nightcycle_cold-fusion@9.0.7"]["cold-fusion"])
-export type State<T> = REQUIRED_MODULE.State<T>
-export type ValueState<T> = REQUIRED_MODULE.ValueState<T>
-export type CanBeState<T> = REQUIRED_MODULE.CanBeState<T>
-export type Fuse = REQUIRED_MODULE.Fuse 
-return REQUIRED_MODULE
