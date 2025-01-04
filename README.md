d99d878ea8f689581c684af94cf1dd2a8b65523a
Nightcycle <77173389+nightcycle@users.noreply.github.com>
2025-01-03 17:36:53 -0700

    modernized workflow + cleaned up dev env

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # page-service
+
 A quick to deploy HUD that blends in with the CoreGui
--- scripts/serve.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-rojo serve dev.project.json
-rojo build dev.project.json --output dev.rbxl
